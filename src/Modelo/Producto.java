ac1b2ae632af1d246ec646af416d81accbe045ce
maicol camacho <maicol2003camacho@gmail.com>
2025-01-15 14:03:53 +0100

    añadir clase producto

---
+++ src/Modelo/Producto.java
@@ -0,0 +1,71 @@
+
+package Modelo;
+
+import java.util.Date;
+
+/**
+ *
+ * @author maicol
+ */
+public class Producto {
+  private String nombre;
+  private int cantidad;
+  private double precio;
+  private double descuento;
+  private Date fecha;
+
+    public Producto(String nombre, int cantidad, double precio, double descuento, Date fecha) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.descuento = descuento;
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+  
+
+   
+    
+    
+    
+}
