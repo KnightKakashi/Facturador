6c9e799b64cadbc7982aeabccfd9a60de86584cc
elotefrutos <elotefrutos@gmail.com>
2025-01-16 17:51:18 +0100

    Implementacion vista de previsualización de la factura

--- src/Vista/MiVista.java
+++ src/Vista/MiVista.java
@@ -219,6 +219,11 @@ public class MiVista extends javax.swing.JFrame {
         jButtonGuardar.setText("GUARDAR");
         jButtonGuardar.setToolTipText("Exportar la factura en formato PDF y XML.");
         jButtonGuardar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+        jButtonGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGuardarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanelProductoLayout = new javax.swing.GroupLayout(jPanelProducto);
         jPanelProducto.setLayout(jPanelProductoLayout);
@@ -288,6 +293,12 @@ public class MiVista extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonGuardarActionPerformed
+       MiFacturita aaa = new MiFacturita();
+        aaa.setVisible(true);
+    }//GEN-LAST:event_jButtonGuardarActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonAñadir;
     private javax.swing.JButton jButtonGuardar;
