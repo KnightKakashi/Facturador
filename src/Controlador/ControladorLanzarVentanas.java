e3481ef994ac4451d310fc33c664ebc01c61fb7e
javiersilvero207 <javiersilvero207@localhost>
2025-01-19 02:06:51 +0100

    Metodo para revisar si los campos de miVista estan vacios y he borrado el boton guardar cliente y he implementado los metodos en guardar COLABORADOR

--- src/Controlador/ControladorLanzarVentanas.java
+++ src/Controlador/ControladorLanzarVentanas.java
@@ -249,7 +249,7 @@ public class ControladorLanzarVentanas implements ActionListener {
                 rellenarProductosPDF();
                 rellenarClientePDF(); 
                 System.out.println("Sacando factura...");
-                lanzarVentanaFactura();
+                lanzarVentanaFactura() ;
                 }
                 break;
             case "IMPRIMIR":
